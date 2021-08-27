a597bbf375b4d3b151581ab67699e92e7df05a7c
CarlosJHR64 <carlosjhr64@gmail.com>
2021-08-27 10:33:26 -0700

    restart fixes

--- lib/mdserver/plug/restart.rb
+++ lib/mdserver/plug/restart.rb
@@ -1,24 +1,17 @@
 module MDServer
 class Base
-  module Restart
-    TIME = Time.now
-  end
-
   get '/restart.html' do
-    version = VERSION
-    if File.mtime(__FILE__) > Restart::TIME
-      version = 'Restarting...'
-      Thread.new do
-        sleep 1
-        Kernel.exec(__FILE__)
-      end
+    Thread.new do
+      sleep 1
+      Kernel.exec($0, *ARGV)
     end
     <<~RESTART
       <!DOCTYPE html>
       <html>
       <head><title>restart</title></head>
       <body>
-      <h1>#{version}</h1>
+      <h1>MDSever restart</h1>
+      <pre>#{$0} #{ARGV.join(' ')}</pre>
       </body>
       </html>
     RESTART
