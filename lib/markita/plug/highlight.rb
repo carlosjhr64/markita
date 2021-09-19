ef784cfbcc8729611b8e1d0b4aa64fdc52941057
CarlosJHR64 <carlosjhr64@gmail.com>
2021-09-18 19:31:45 -0700

    Rouge themes are back!

--- lib/markita/plug/highlight.rb
+++ lib/markita/plug/highlight.rb
@@ -2,7 +2,9 @@ module Markita
 class Base
   HEADER_LINKS << %Q(  <link rel="stylesheet" href="/highlight.css" type="text/css">\n)
   module Highlight
-    CSS = File.read PATH['highlight.css']
+    theme = OPTIONS&.theme || 'base16.light'
+    CSS = Rouge::Theme.find(theme)&.render(scope: '.highlight')
+    raise "Can't find Rouge Theme "+theme  unless CSS
   end
 
   get '/highlight.css' do
