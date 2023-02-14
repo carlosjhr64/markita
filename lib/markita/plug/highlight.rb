c7f4d10a4b14a28420c43abe7ed25a92e2fb1865
CarlosJHR64 <carlosjhr64@gmail.com>
2023-02-14 12:57:37 -0800

    rubocoped

--- lib/markita/plug/highlight.rb
+++ lib/markita/plug/highlight.rb
@@ -1,10 +1,11 @@
 module Markita
 class Base
-  HEADER_LINKS << %Q(  <link rel="stylesheet" href="/highlight.css" type="text/css">\n)
+  HEADER_LINKS <<
+    %(  <link rel="stylesheet" href="/highlight.css" type="text/css">\n)
   module Highlight
     theme = OPTIONS&.theme || 'base16.light'
     CSS = Rouge::Theme.find(theme)&.render(scope: '.highlight')
-    raise "Can't find Rouge Theme "+theme  unless CSS
+    raise "Can't find Rouge Theme "+theme unless CSS
   end
 
   get '/highlight.css' do
