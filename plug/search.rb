aae100c1413bb0cf43b8693c68497467875491b6
CarlosJHR64 <carlosjhr64@gmail.com>
2024-10-01 12:36:01 -0700

    More context, no match message

--- plug/search.rb
+++ plug/search.rb
@@ -6,9 +6,9 @@ class Base
       text << "+ Keywords:\n"
       text << "+ #{keywords.join(' ')}\n"
 
-      section,file,matches = '','',[]
-      primary = keywords.max_by(&:length)
-      `egrep -r --include='*.md' -A 1 -B 1 -i '\\b#{primary}\\b' #{ROOT}`
+      section,file,matches = '',nil,[]
+      primary = keywords.first
+      `egrep -r --include='*.md' -A 2 -B 2 -i '\\b#{primary}\\b' #{ROOT}`
       .each_line do |line|
         if line=~/^-/
           matches.push [file, section]
@@ -20,20 +20,23 @@ class Base
           section << line unless line.strip.empty?
         end
       end
-      matches.push [file, section]
+      matches.push [file, section] if file
 
-      keywords = keywords.map{/\b(#{_1})\b/i}
-      max = matches.map{|fs| keywords.count{_1.match?(fs[1])}}.max
-
-      matches.each do |file, section|
-        next if keywords.count{_1.match?(section)} < max
-        text << %(<a href="#{file}">#{file}:</a>\n)
-        text << "```\n"
-        keywords.each do |keyword|
-          section = section.gsub(keyword, '<mark>\1</mark>')
+      if matches.empty?
+        text << "* No matches for primary keyword: <mark>#{primary}</mark>\n"
+      else
+        keywords = keywords.map{/\b(#{_1})\b/i}
+        max = matches.map{|fs| keywords.count{_1.match?(fs[1])}}.max
+        matches.each do |file, section|
+          next if keywords.count{_1.match?(section)} < max
+          text << %(<a href="#{file}">#{file}:</a>\n)
+          text << "```\n"
+          keywords.each do |keyword|
+            section = section.gsub(keyword, '<mark>\1</mark>')
+          end
+          text << section
+          text << "```\n"
         end
-        text << section
-        text << "```\n"
       end
     end
     Markdown.new('Search').markdown text
