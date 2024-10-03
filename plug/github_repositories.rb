0ca52a6ca53a538f330c858ec38c992266ff32b3
CarlosJHR64 <carlosjhr64@gmail.com>
2024-10-03 14:27:19 -0700

    Check last release

--- plug/github_repositories.rb
+++ plug/github_repositories.rb
@@ -30,6 +30,12 @@ class Base
     repos = JSON.parse Net::HTTP.get(
       URI "#{url}?&sort=#{sort}&direction=#{direction}")
     repos.each do |repo|
+      if repo.is_a? String
+        text << repo
+        text << "\n"
+        break
+      end
+      name = repo['name']
       stars,issues = '',''
       if (n=repo['watchers'].to_i).positive?
         n = Math.log(n+1, 2).round
@@ -39,13 +45,25 @@ class Base
         n = Math.log(n+1, 2).round
         issues = ':heavy_exclamation_mark:'*n
       end
-      text << "+ [#{repo['name']}](#{repo['html_url']})#{stars}#{issues}:\n"
+      text << "+ [#{name}](#{repo['html_url']})#{stars}#{issues}:\n"
       text << "+ #{repo['description']}\n"
       text << "+ #{repo['language']} project created " \
               "#{Date.parse(repo['created_at'])} "
       date = Date.parse repo['pushed_at']
-      text << (today-date>365 || !issues.empty? ? %(last pushed <mark>#{date}</mark>\n) :
-                               %(last pushed #{date}\n))
+      if !issues.empty? || today - date > 365
+        text << "last pushed <mark>#{date}</mark>\n"
+      else
+        latest = JSON.parse Net::HTTP.get(
+          URI "https://api.github.com/repos/#{username}/#{name}/releases/latest")
+        if latest.key?('published_at')
+          date = Date.parse latest['published_at']
+          text << (today - date > 365 ?
+                  "last release <mark>#{date}</mark>\n" :
+                  "last release #{date}\n")
+        else
+          text << "last pushed #{date}\n"
+        end
+      end
     end
 
     Markdown.new('Github Repositories').markdown text
