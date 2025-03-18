a57f42c4c069747153e716043672df677b38a96a
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-18 14:42:29 -0700

    Rubocop review html.rb

--- lib/markita/html.rb
+++ lib/markita/html.rb
@@ -1,25 +1,29 @@
+# frozen_string_literal: true
+
+# Markita namespace
 module Markita
-module HTML
-  def self.header(title)
-    <<~HEADER
-      <!DOCTYPE html>
-      <html>
-      <head>
-      <title>#{title}</title>#{HEADER_LINKS}
-      </head>
-      <body>
-    HEADER
-  end
+  # HTML template
+  module HTML
+    def self.header(title)
+      <<~HEADER
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>#{title}</title>#{HEADER_LINKS}
+        </head>
+        <body>
+      HEADER
+    end
 
-  def self.navigation
-    NAVIGATION
-  end
+    def self.navigation
+      NAVIGATION
+    end
 
-  def self.footer
-    <<~FOOTER
-      </body>
-      </html>
-    FOOTER
+    def self.footer
+      <<~FOOTER
+        </body>
+        </html>
+      FOOTER
+    end
   end
-end
 end
