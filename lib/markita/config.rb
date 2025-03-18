ce51dff25b0e892a268d3cab24a55ce4e0052820
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-18 14:33:58 -0700

    Rubocop review config.rb

--- lib/markita/config.rb
+++ lib/markita/config.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
+# Markita namespace
 module Markita
-  HEADER_LINKS = ''
-  NAVIGATION   = ''
+  HEADER_LINKS = String.new
+  NAVIGATION   = String.new
 
   BIND   = '0.0.0.0'
   PORT   = '8080'
@@ -10,19 +13,27 @@ module Markita
   INDEX = 'index'
 
   ROOT = File.expand_path OPTIONS.root || '~/vimwiki'
-  raise 'Missing site root directory: '+ROOT unless File.directory? ROOT
+  raise "Missing site root directory: #{ROOT}" unless File.directory? ROOT
 
   SSL_CERTIFICATE = File.join(ROOT, '.cert.crt')
   SSL_PRIVATE_KEY = File.join(ROOT, '.pkey.pem')
 
-  SERVER_SETTINGS = [SSL_CERTIFICATE, SSL_PRIVATE_KEY].all?{File.exist?_1} ?
-    { SSLEnable: true,
-      SSLVerifyClient: OpenSSL::SSL::VERIFY_NONE,
-      SSLCertificate: OpenSSL::X509::Certificate.new(File.read SSL_CERTIFICATE),
-      SSLPrivateKey:  OpenSSL::PKey::RSA.new(File.read SSL_PRIVATE_KEY) } : nil
+  SERVER_SETTINGS =
+    if [SSL_CERTIFICATE, SSL_PRIVATE_KEY].all? { File.exist? it }
+      {
+        SSLEnable:
+          true,
+        SSLVerifyClient:
+          OpenSSL::SSL::VERIFY_NONE,
+        SSLCertificate:
+          OpenSSL::X509::Certificate.new(File.read(SSL_CERTIFICATE)),
+        SSLPrivateKey:
+          OpenSSL::PKey::RSA.new(File.read(SSL_PRIVATE_KEY))
+      }
+    end
 
   SERVER_CONFIG = lambda do |server|
-    puts "#{$0}-#{VERSION}".blue
+    puts "#{$PROGRAM_NAME}-#{VERSION}".blue
     puts "Sinatra-#{Sinatra::VERSION} using #{server.class}".blue
   end
 
@@ -30,7 +41,7 @@ module Markita
   APPDATA = File.join APPDIR, 'data'
 
   PATH = lambda do |basename|
-    [ROOT, APPDATA].map{ File.join _1, basename}.detect{ File.exist? _1}
+    [ROOT, APPDATA].map { File.join it, basename }.detect { File.exist? it }
   end
 
   NOT_FOUND = File.read PATH['not_found.html']
--- lib/markita/requires.rb
+++ lib/markita/requires.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Standard libraries
+require 'English'
 require 'digest'
 require 'cgi'
 require 'openssl'
