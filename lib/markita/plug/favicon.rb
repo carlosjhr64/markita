1fe7629ec4d1ada5c72b380636d324859702340e
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-19 13:53:23 -0700

    Rubocop review of fabicon and highight

--- lib/markita/plug/favicon.rb
+++ lib/markita/plug/favicon.rb
@@ -1,15 +1,19 @@
+# frozen_string_literal: true
+
+# Markita namespace
 module Markita
-class Base
-  HEADER_LINKS <<
-    %(\n<link rel="icon" type="image/x-icon" href="/favicon.ico">)
+  # Base class of the Sinatra Markita application
+  class Base
+    HEADER_LINKS <<
+      %(\n<link rel="icon" type="image/x-icon" href="/favicon.ico">)
 
-  module Favicon
-    ICO = File.read PATH['favicon.ico']
-  end
+    module Favicon
+      ICO = File.read PATH['favicon.ico']
+    end
 
-  get '/favicon.ico' do
-    headers 'Content-Type' => 'image/x-icon'
-    Favicon::ICO
+    get '/favicon.ico' do
+      headers 'Content-Type' => 'image/x-icon'
+      Favicon::ICO
+    end
   end
-end
 end
--- lib/markita/plug/highlight.rb
+++ lib/markita/plug/highlight.rb
@@ -1,17 +1,21 @@
+# frozen_string_literal: true
+
+# Markita namespace
 module Markita
-class Base
   HEADER_LINKS <<
     %(\n<link rel="stylesheet" href="/highlight.css" type="text/css">)
+  # Base class of the Sinatra Markita application
+  class Base
+    # Highlight namespace to support the /highlight.css route
+    module Highlight
+      theme = OPTIONS.theme || 'base16.light'
+      CSS = Rouge::Theme.find(theme)&.render(scope: '.highlight')
+      raise "Can't find Rouge Theme "+theme unless CSS
+    end
 
-  module Highlight
-    theme = OPTIONS.theme || 'base16.light'
-    CSS = Rouge::Theme.find(theme)&.render(scope: '.highlight')
-    raise "Can't find Rouge Theme "+theme unless CSS
+    get '/highlight.css' do
+      headers 'Content-Type' => 'text/css'
+      Highlight::CSS
+    end
   end
-
-  get '/highlight.css' do
-    headers 'Content-Type' => 'text/css'
-    Highlight::CSS
-  end
-end
 end
