6d9965188a35c478f6d7f8ffe82483c0bd0912d8
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-19 14:02:03 -0700

    Rubocop review login.rb

--- lib/markita/plug/login.rb
+++ lib/markita/plug/login.rb
@@ -6,25 +6,27 @@ module Markita
   class Base
     # Login namespace to support the /login.html route
     module Login
-      ID = File.exist?(_=File.join(ROOT, '.valid-id')) ?
-           File.read(_).strip :
-           nil
-      IPS = (_=OPTIONS.allowed)? _.split(',') : []
+      ID = if File.exist?(filename = File.join(ROOT, '.valid-id'))
+             File.read(filename).strip
+           end
+      IPS = (ips = OPTIONS.allowed) ? ips.split(',') : []
       if !IPS.empty? && !ID
         raise 'Allowed ips without site password does not make sense.'
       end
+
       FORM   = File.read PATH['login_form.html']
       FAILED = File.read PATH['login_failed.html']
     end
 
     before do
       unless Login::ID.nil? || Login::IPS.include?(request.ip)
-        if (id=params[:id])
+        if (id = params[:id])
           session[:id] = Digest::SHA256.hexdigest id
         end
         if session[:id] == Login::ID
           redirect '/' if request.path_info == '/login.html'
         else
+          # Report this unauthorized access attempt
           puts "#{request.ip} #{request.path_info}".magenta
           redirect '/login.html' unless request.path_info == '/login.html'
         end
