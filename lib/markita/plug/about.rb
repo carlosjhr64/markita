02ed836d0268a2dea7eba70c3a51b64c79a358d0
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-19 13:48:55 -0700

    Rubocop review of plug/about.rb

--- lib/markita/plug/about.rb
+++ lib/markita/plug/about.rb
@@ -1,33 +1,44 @@
+# frozen_string_literal: true
+
+# Markita namespace
 module Markita
-class Base
-  module About
-    def self.page
-      text = <<~TEXT
+  # Base class of the Sinatra Markita application
+  class Base
+    # About namespace to support the /about.html route
+    module About
+      ABOUT_TEXT = <<~TEXT.freeze
         # [Markita](https://github.com/carlosjhr64/markita)
 
         * VERSION: #{VERSION}
-        * ROOT: #{ROOT.sub(%r{^/home/\w+/},'~/')}
+        * ROOT: #{ROOT.sub(%r{^/home/\w+/}, '~/')}
         * ARGV: #{ARGV.join(' ')}
         * START_TIME: #{START_TIME}
 
         ## Plug.html routes:
 
       TEXT
-      Base.routes['GET'].each do |route|
-        path = route[0].to_s
-        next unless %r{^/\w+\.html$}.match? path
-        basename = File.basename(path, '.*')
-        text << "* [#{basename}](#{path})\n"
+
+      def self.plugs
+        Base.routes['GET'].each do |route|
+          path = route[0].to_s
+          next unless %r{^/\w+\.html$}.match? path
+
+          yield path, File.basename(path, '.*')
+        end
       end
-      if defined? Favicon && Favicon::ICO
-        text << "\n![Favicon](/favicon.ico)\n"
+
+      def self.page
+        text = ABOUT_TEXT.dup
+        plugs { |path, basename| text << "* [#{basename}](#{path})\n" }
+        if defined? Favicon && Favicon::ICO
+          text << "\n![Favicon](/favicon.ico)\n"
+        end
+        text
       end
-      text
+    end
+
+    get '/about.html' do
+      Markdown.new('About').markdown About.page
     end
   end
-
-  get '/about.html' do
-    Markdown.new('About').markdown About.page
-  end
-end
 end
