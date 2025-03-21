60c96258a6a9fc14136f1d4439cab62a1e4acd86
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-21 15:46:01 -0700

    form.rb review

--- lib/markita/markdown/form.rb
+++ lib/markita/markdown/form.rb
@@ -11,56 +11,76 @@ module Markita
       field = '(\w+:)?\[(\*)?(\w+)(=("[^"]+")(,"[^"]+")*)?\]'
       RGX = Regexp.new "^!( #{field})+"
       FIELD = Regexp.new field
+      POST = /!$/
+      ACTION = /\(([^()]*)\)!?$/
+
+      # :reek:LongParameterList :reek:DuplicateMethodCall
+      # :reek:TooManyStatements
+      # rubocop:disable Metrics/MethodLength, Metrics/PerceivedComplexity
+      def self.input(type, field, name, values)
+        if field
+          if values.empty?
+            %(  #{field}:<input type="#{type}" name="#{name}">\n)
+          elsif values.count > 1
+            %(  #{field}:<select name="#{name}">\n).tap do |html|
+              values.each do |value|
+                html << %(  <option value="#{value}">#{value}</option>\n)
+              end
+              html << "</select>\n"
+            end
+          else
+            <<-INPUT
+  #{field}:<input type="#{type}" name="#{name}"
+    value="#{values[0]}">
+            INPUT
+          end
+        elsif name == 'submit'
+          %(  <input type="submit" value="#{values[0] || 'Submit'}">\n)
+        else
+          %(  <input type="hidden" name="#{name}" value="#{values[0]}">\n)
+        end
+      end
+
+      def self.match?(line) = RGX.match?(line)
+
+      # :reek:LongYieldList :reek:TooManyStatements
+      def self.scan(line)
+        line.scan(FIELD).each do |field, pwd, name, value|
+          field &&= field[0...-1]
+          values = value ? value[2...-1].split('","') : []
+          type = pwd ? 'password' : 'text'
+          yield field, type, name, values
+        end
+      end
+
+      def self.start(line, attributes)
+        method = POST.match?(line) ? ' method="post"' : ''
+        action = (mdt = ACTION.match(line)) ? %( action="#{mdt[1]}") : ''
+        %(<form#{action}#{method}#{attributes.shift}>\n)
+      end
+
+      def self.stop = %(</form>\n)
     end
 
     PARSERS << :form
 
+    # category: method
+    # :reek:TooManyStatements
     def form
-      return false unless (continue = Form::RGX.match(@line))
+      return false unless Form.match?(@line)
 
-      fields,nl,submit = 0,false,nil
-      action = (_=/\(([^()]*)\)!?$/.match(@line))? %( action="#{_[1]}") : nil
-      method = @line.match?(/!$/) ? ' method="post"' : nil
-      @html << %(<form#{action}#{method}#{@attributes.shift}>\n)
-      while continue
-        @html << "  <br>\n" if nl
-        @line.scan(Form::FIELD).each do |field, pwd, name, value|
-          field &&= field[0...-1]
-          value &&= value[2...-1]
-          if field
-            type = pwd ? 'password' : 'text'
-            if value
-              if (values = value.split('","')).length > 1
-                @html << %(#{field}:<select name="#{name}">\n)
-                values.each do |value|
-                  fields += 1
-                  @html << %(  <option value="#{value}">#{value}</option>\n)
-                end
-                @html << "</select>\n"
-              else
-                fields += 1
-                @html << %(  #{field}:<input type="#{type}" name="#{name}")
-                @html << %( value="#{value}">\n)
-              end
-            else
-              fields += 1
-              @html << %(  #{field}:<input type="#{type}" name="#{name}">\n)
-            end
-          elsif name=='submit'
-            submit = value
-          else
-            @html << %(  <input type="hidden" name="#{name}" value="#{value}">\n)
-          end
+      @html << Form.start(@line, @attributes)
+      loop do
+        Form.scan(@line) do |field, type, name, values|
+          @html << Form.input(type, field, name, values)
         end
-        continue = Form::RGX.match?(@line = @file.gets) and nl ||= true
+        break unless Form.match?(@line = @file.gets)
+
+        @html << "  <br>\n"
       end
-      if submit || fields!=1
-        submit ||= 'Submit'
-        @html << "  <br>\n" if nl
-        @html << %(  <input type="submit" value="#{submit}">\n)
-      end
-      @html << %(</form>\n)
+      @html << Form.stop
       true
     end
+    # rubocop:enable Metrics/MethodLength, Metrics/PerceivedComplexity
   end
 end
