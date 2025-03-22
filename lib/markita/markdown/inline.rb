c91f56942b2edc529b856dc716fafa786dd8e9ad
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-22 13:03:00 -0700

    mv inline. TODO: metadata

---
+++ lib/markita/markdown/inline.rb
@@ -0,0 +1,111 @@
+# frozen_string_literal: true
+
+# Markita top level namespace
+module Markita
+  # Markdown namespace
+  # :reek:InstanceVariableAssumption in markdown.rb
+  class Markdown
+    # Module to isolate from Markdown
+    # :reek:TooManyConstants
+    module Inline
+      # category: details
+
+      ENTITY = /\\([<>*"~`_&;:\\])/
+      def self.entity(mdt) = "&##{mdt[1].ord};"
+
+      FOOTNOTE = /\[\^(\d+)\](:)?/
+      # :reek:ControlParameter
+      def self.footnote(mdt = nil, num: mdt[1], ref: mdt[2])
+        if ref
+          %(<a id="fn:#{num}" href="#fnref:#{num}">#{num}:</a>)
+        else
+          %(<a id="fnref:#{num}" href="#fn:#{num}"><sup>#{num}</sup></a>)
+        end
+      end
+
+      SUBSCRIPT = /\\\(([^()]+)\)/
+      def self.subscript(mdt) = "<sub>#{mdt[1]}</sub>"
+
+      SUPERSCRIPT = /\\\^\(([^()]+)\)/
+      def self.superscript(mdt) = "<sup>#{mdt[1]}</sup>"
+
+      URL = %r{(https?://[\w./&+?%-]+)}
+      def self.url(mdt, hrf = mdt[1]) = %(<a href="#{hrf}">#{hrf}</a>)
+
+      BOLD = /\*([^*]+)\*/
+      def self.bold(mdt) = "<b>#{mdt[1]}</b>"
+
+      CODE = /`([^`]+)`/
+      def self.code(mdt) = "<code>#{mdt[1].gsub('<', '&lt;')}</code>"
+
+      EMOJI = /:(\w+):/
+      def self.emoji(mdt) = (emj = EMOJIS[mdt[1]]) ? "&#x#{emj};" : mdt[0]
+
+      ITALIC = /"([^"]+)"/
+      def self.italic(mdt) = "<i>#{mdt[1]}</i>"
+
+      STRIKE = /~([^~]+)~/
+      def self.strike(mdt) = "<s>#{mdt[1]}</s>"
+
+      UNDERLINE = /_([^_]+)_/
+      def self.underline(mdt) = "<u>#{mdt[1]}</u>"
+
+      ANCHOR = /\[([^\[\]]+)\]\(([^()]+)\)/
+      def self.anchor(mdt)
+        # TODO: metadata
+        # href = (((_ = mdt[2]).match?(/^\d+$/) and @metadata[_]) or _)
+        href = mdt[2]
+        text = tag(mdt[1], EMOJI, method(:emoji))
+        %(<a href="#{href}">#{text}</a>)
+      end
+
+      # category: algorithm
+
+      # :reek:DuplicateMethodCall :reek:TooManyStatements
+      # rubocop:disable Metrics/MethodLength
+      def self.tag(entry, regx, m2string, &block)
+        if (mdt = regx.match entry)
+          string = String.new
+          while mdt
+            pre_match = (block ? block.call(mdt.pre_match) : mdt.pre_match)
+            string << (pre_match + m2string[mdt])
+            post_match = mdt.post_match
+            mdt = regx.match(post_match)
+          end
+          string << (block ? block.call(post_match) : post_match)
+          return string
+        end
+        block ? block.call(entry) : entry
+      end
+
+      # :reek:NestedIterators :reek:TooManyStatements
+      # :reek:UncommunicativeVariableName
+      # rubocop:disable Metrics/AbcSize
+      def self.tags(line)
+        line = tag(line, ENTITY, method(:entity))
+        line = tag(line, CODE, method(:code)) do |string|
+          tag(string, ANCHOR, method(:anchor)) do |str| # TODO: anchor issue
+            tag(str, URL, method(:url)) do |s|
+              s = tag(s, EMOJI,       method(:emoji))
+              s = tag(s, BOLD,        method(:bold))
+              s = tag(s, ITALIC,      method(:italic))
+              s = tag(s, STRIKE,      method(:strike))
+              s = tag(s, UNDERLINE,   method(:underline))
+              s = tag(s, FOOTNOTE,    method(:footnote))
+              s = tag(s, SUPERSCRIPT, method(:superscript))
+              tag(s, SUBSCRIPT, method(:subscript))
+            end
+          end
+        end
+        line.sub(/ ?[ \\]$/, '<br>')
+      end
+      # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/MethodLength
+    end
+
+    # category: method
+
+    # :reek:UtilityFunction
+    def inline(line) = Inline.tags(line)
+  end
+end
