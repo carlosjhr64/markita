22b84c5bab4beea9a0339b37d3790fec6aa19c18
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-25 09:38:22 -0700

    Progress on github_repositories.html review.

--- plug/github_repositories.rb
+++ plug/github_repositories.rb
@@ -6,37 +6,35 @@ require 'net/http'
 module Markita
   # Base class for Sinatra
   class Base
-    # Module for code isolation
-    module GithubRepositories
+    # Github repositories reports
+    # :reek:TooManyInstanceVariables
+    class GithubRepositories
       # :reek:TooManyStatements
-      # rubocop: disable Metrics/MethodLength
-      def self.intro(sort, direction, username)
-        text = String.new
-        text << "# Github Repositories\n"
+      # rubocop: disable Metrics/MethodLength, Layout/LineLength
+      def intro
+        @text << "# Github Repositories\n"
 
-        text << %(! Username:[username="#{username}"])
-        text << %([sort="#{sort}"][direction="#{direction}"][submit="Go!"])
-        text << "()\n"
+        @text << %(! Username:[username="#{@username}"])
+        @text << %([sort="#{@sort}"][direction="#{@direction}"][submit="Go!"])
+        @text << "()\n"
 
-        text << '* Sort [pushed]'
-        text << "(?username=#{username}&sort=pushed&direction=#{direction}) "
-        text << 'or [created]'
-        text << "(?username=#{username}&sort=created&direction=#{direction})\n"
+        @text << '* Sort [pushed]'
+        @text << "(?username=#{@username}&sort=pushed&direction=#{@direction}) "
+        @text << 'or [created]'
+        @text << "(?username=#{@username}&sort=created&direction=#{@direction})\n"
 
-        text << '* Direction [ascending]'
-        text << "(?username=#{username}&sort=#{sort}&direction=asc) "
-        text << 'or [descending]'
-        text << "(?username=#{username}&sort=#{sort}&direction=desc)\n"
+        @text << '* Direction [ascending]'
+        @text << "(?username=#{@username}&sort=#{@sort}&direction=asc) "
+        @text << 'or [descending]'
+        @text << "(?username=#{@username}&sort=#{@sort}&direction=desc)\n"
 
-        text << "## Repo #{username} sorted by #{sort}(#{direction}.)\n"
-
-        text
+        @text << "## Repo #{@username} sorted by #{@sort}(#{@direction}.)\n"
       end
 
       # :reek:DuplicateMethodCall :reek:TooManyStatements
       # :reek:UncommunicativeVariableName
       # rubocop: disable Metrics/AbcSize, Metrics/PerceivedComplexity
-      def self.report(repo, today, text)
+      def report(repo)
         name = repo['name']
         stars = ''
         issues = ''
@@ -52,48 +50,51 @@ module Markita
           n = Math.log(n + 1, 2).round
           issues = ':heavy_exclamation_mark:' * n
         end
-        text << "+ [#{name}](#{repo['html_url']})#{stars}#{issues}:\n"
-        text << "+ #{repo['description']}\n"
-        text << "+ #{repo['language']} project created " \
-                "#{Date.parse(repo['created_at'])} "
+        @text << "+ [#{name}](#{repo['html_url']})#{stars}#{issues}:\n"
+        @text << "+ #{repo['description']}\n"
+        @text << "+ #{repo['language']} project created #{Date.parse(repo['created_at'])} "
         date = Date.parse repo['pushed_at']
-        if !issues.empty? || today - date > 365
-          text << "last pushed <mark>#{date}</mark>\n"
+        if !issues.empty? || @today - date > 365
+          @text << "last pushed <mark>#{date}</mark>\n"
         else
           latest = JSON.parse Net::HTTP.get(
-            URI("https://api.github.com/repos/#{username}/#{name}/releases/latest")
+            URI("https://api.github.com/repos/#{@username}/#{name}/releases/latest")
           )
           if latest.key?('published_at')
             date = Date.parse latest['published_at']
-            text << (if today - date > 365
+            @text << if @today - date > 365
                        "last release <mark>#{date}</mark>\n"
                      else
                        "last release #{date}\n"
-                     end)
+                     end
           else
-            text << "last pushed #{date}\n"
+            @text << "last pushed #{date}\n"
           end
         end
-        text
       end
       # rubocop: enable Metrics/AbcSize, Metrics/PerceivedComplexity
 
       # :reek:TooManyStatements
-      def self.text(sort, direction, username)
-        url = "https://api.github.com/users/#{username}/repos?per_page=100&"
+      def text
+        url = "https://api.github.com/users/#{@username}/repos?per_page=100&"
         repos = JSON.parse Net::HTTP.get(
-          URI("#{url}?&sort=#{sort}&direction=#{direction}")
+          URI("#{url}?&sort=#{@sort}&direction=#{@direction}")
         )
-        if (text = repos['message'])
-          return text
-        end
+        return repos['message'] if repos.is_a?(Hash)
 
-        text = intro(sort, direction, username)
-        today = Date.today
-        repos.each { report(it, today, text) }
-        text
+        intro
+        repos.each { report(it) }
+        @text
       end
-      # rubocop: enable Metrics/MethodLength
+      # rubocop: enable Metrics/MethodLength, Layout/LineLength
+
+      def initialize(sort, direction, username)
+        @sort = sort
+        @direction = direction
+        @username = username
+        @text = String.new
+        @today = Date.today
+      end
     end
 
     get '/github_repositories.html' do
@@ -101,7 +102,7 @@ module Markita
       direction = params['direction'] || 'asc'
       # Change the username to yours!
       username = params['username'] || 'carlosjhr64'
-      text = GithubRepositories.text(sort, direction, username)
+      text = GithubRepositories.new(sort, direction, username).text
       Markdown.new('Github Repositories').markdown text
     end
   end
