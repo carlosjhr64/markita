ef8d746e0a55e46be3429754b20151a1fb4f57b3
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-25 06:36:50 -0700

    restart.html review

--- plug/restart.rb
+++ plug/restart.rb
@@ -1,22 +1,24 @@
-# NOTE: Only for password protected personal website
-# as anyone can trigger this.
+# frozen_string_literal: true
+
+# NOTE: Only for password protected personal website as anyone can trigger this.
 module Markita
-class Base
-  get '/restart.html' do
-    Thread.new do
-      sleep 1
-      Kernel.exec($0, *ARGV)
+  # Base class for Sinatra
+  class Base
+    get '/restart.html' do
+      Thread.new do
+        sleep 1
+        Kernel.exec($PROGRAM_NAME, *ARGV)
+      end
+      <<~RESTART
+        <!DOCTYPE html>
+        <html>
+        <head><title>restart</title></head>
+        <body>
+        <h1>MDServer restart</h1>
+        <pre>#{$PROGRAM_NAME} #{ARGV.join(' ')}</pre>
+        </body>
+        </html>
+      RESTART
     end
-    <<~RESTART
-      <!DOCTYPE html>
-      <html>
-      <head><title>restart</title></head>
-      <body>
-      <h1>MDServer restart</h1>
-      <pre>#{$0} #{ARGV.join(' ')}</pre>
-      </body>
-      </html>
-    RESTART
   end
-end
 end
