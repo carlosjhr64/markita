c8fad5990c048539da5b8d77580010b5eef58617
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-26 08:08:08 -0700

    Option --no can disable custom plugs also.

--- lib/markita/plug/plugs.rb
+++ lib/markita/plug/plugs.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-Dir.glob(File.join(Markita::ROOT, 'plug', '*.rb')).each { require it }
+Dir.glob(File.join(Markita::ROOT, 'plug', '*.rb')).each do |path|
+  next if Markita.no.include? File.basename(path, '.rb').to_sym
+
+  require path
+end
